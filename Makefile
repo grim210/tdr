b4892e946d20c8faf9f0cb10cdd1227515a547f9
Joe Brown <brownj3199@live.com>
2016-12-15 01:25:54 -0600

    Splitting the project into modules.
    Right now the the renderer module is serving as the main module and rendering
    module...but not for long.

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 all:
+	cd main; make
 	cd renderer; make
+	cd test; make
 
 clean:
+	cd main; make clean
 	cd renderer; make clean
+	cd test; make clean
 
 distclean:
+	cd main; make distclean
 	cd renderer; make distclean
+	cd test; make distclean
---
+++ test/main.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <camera.h>
+#include <cube.h>
+#include <directdrawtexture.h>
+#include <window.h>
+
+int main(int argc, char* argv[])
+{
+    std::unique_ptr<Window> window = Window::Initialize(800, 600, false);
+    if (window == nullptr) {
+        std::cerr << "nullptr returned.  Aborting." << std::endl;
+        return -1;
+    }
+    window->setClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+    std::shared_ptr<DirectDrawTexture> ddtex(
+        new DirectDrawTexture("textures/uvtemplate.dds"));
+    std::shared_ptr<Cube> cube = Cube::Create(ddtex);
+    std::shared_ptr<Camera> camera = Camera::Create(cube);
+
+    glm::mat4 proj = glm::perspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
+
+    bool running = true;
+    while (running) {
+        double elapsed = glfwGetTime();
+
+        cube->update(elapsed, camera->getView(), proj);
+        window->clear();
+        cube->draw();
+        running = window->swap();
+    }
+
+    Cube::Destroy(cube.get());
+    Window::Destroy(window.get());
+    return 0;
+}
