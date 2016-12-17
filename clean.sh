9133c024593ceb6131319de767b67c4a10439f16
Joe Brown <brownj3199@live.com>
2016-12-17 03:51:00 -0600

    Autotools work.  libmain.a builds successfully now.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DELSTRING="aclocal.m4 ar-lib autom4te.cache compile config.h config.h.in config.guess config.log config.status config.sub configure depcomp install-sh libtool ltmain.sh Makefile Makefile.in missing stamp-h1"
+DELSTRING="aclocal.m4 ar-lib autom4te.cache compile confdefs.h config.h config.h.in config.guess config.log config.status config.sub configure depcomp install-sh libtool ltmain.sh Makefile Makefile.in missing stamp-h1"
 RM="rm -rf"
 M4_GARBAGE="m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4"
 
