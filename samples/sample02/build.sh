52affe345d48e1412f9a062489622479d0cd6388
Joe Brown <brownj3199@live.com>
2016-12-19 23:13:40 -0600

    Build cleanup.
    Both of those annoyances were used to debug the build process to get CMake
    going initially.

--- samples/sample02/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -std=c++11 sample02.cpp -o sample_02 -lGL -lglfw -ltdrmain -ltdrrenderer -ldl -I../../main/include -I../../renderer/include -L../../build/main/src -L../../build/renderer/src
