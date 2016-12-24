fbe254f061bb31077e59d64fc34c60f6aaee2548
Joe Brown <brownj3199@live.com>
2016-12-24 00:24:23 -0600

    Fixed rebase.

--- main/include/tdrmesh.h
+++ main/include/tdrmesh.h
@@ -12,16 +12,17 @@ public:
         UV,
         Vertex
     };
-	TDRMesh(void) { };
-	virtual ~TDRMesh(void) { };
-	static void Delete(std::shared_ptr<TDRMesh> mesh);
-	std::shared_ptr<TDRMesh> Load(const char* buff, size_t len);
+    TDRMesh(void) { };
+    virtual ~TDRMesh(void) { };
+    static void Delete(std::shared_ptr<TDRMesh> mesh);
+    std::shared_ptr<TDRMesh> Load(const char* buff, size_t len);
     std::vector<float> get(Data type);
+    std::vector<float> has(Data type);
 private:
-	std::vector<float> m_colordata;
-	std::vector<float> m_indexdata;
-	std::vector<float> m_uvdata;
-	std::vector<float> m_vertexdata;
+    std::vector<float> m_colordata;
+    std::vector<float> m_indexdata;
+    std::vector<float> m_uvdata;
+    std::vector<float> m_vertexdata;
 };
 
 #endif  /* TDRMAIN_TDRMESH_H */
