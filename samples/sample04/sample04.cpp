37905ad6be08064dffc9d3d4b70a8f8404bf20a6
Joe Brown <brownj3199@live.com>
2017-01-29 22:03:46 -0600

    Added sample04.
    This will be a scene with several cubes doing crazy stuff to demonstrate
    how to control multiple objects within a scene.  Obviously incomplete.

---
+++ samples/sample04/sample04.cpp
@@ -0,0 +1,299 @@
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sstream>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_access.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SDL2/SDL.h>
+
+#include <tdrmain.h>
+#include <tdrrenderer.h>
+
+class Model : public RenderObject {
+public:
+    Model(void) { };
+    virtual ~Model(void) { };
+    static std::shared_ptr<Model> Create(void);
+    static void Delete(std::shared_ptr<Model> model);
+    bool addTexture(std::shared_ptr<DirectDrawTexture> ddt);
+
+    /* overridden from RenderObject */
+    void draw(void);
+    glm::vec3 getPosition(void);
+    bool setPosition(glm::vec3 location);
+    bool translate(glm::vec3 offset);
+    void update(double elapsed, glm::mat4 view, glm::mat4 proj);
+private:
+    std::shared_ptr<ShaderProgram> m_program;
+    std::shared_ptr<GLTexture> m_texture;
+    std::shared_ptr<TDRMesh> m_mesh;
+
+    /* raw vertex and uv data */
+    std::vector<float> m_vertices;
+    std::vector<float> m_uvs;
+
+    GLint m_color_attrib;
+    GLint m_uv_attrib;
+    GLint m_vpos_attrib;
+
+    GLint m_mvp_uniform;
+    GLint m_tex_uniform;
+
+    GLuint m_uvbuffer;
+    GLuint m_vbuffer;
+    GLuint m_texid;
+
+    glm::vec3 m_pos;
+    glm::mat4 m_model;
+    glm::mat4 m_mvp;
+
+    void dump_doubles(std::vector<float> arr);
+    void dump_triplets(std::vector<float> arr);
+
+    /* used in game logic */
+    double last;
+    char direction;
+};
+
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_EVENTS);
+
+    std::unique_ptr<Window> window = Window::Initialize(800, 600, false);
+    if (window == nullptr) {
+        std::cerr << "nullptr returned.  Aborting." << std::endl;
+        return -1;
+    }
+    window->setClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+    std::shared_ptr<Timer> clock = Timer::Create();
+    std::shared_ptr<Model> model = Model::Create();
+    std::shared_ptr<DirectDrawTexture> ddtex(
+        new DirectDrawTexture("textures/uvtemplate.dds"));
+    model->addTexture(ddtex);
+    std::shared_ptr<Camera> camera = Camera::Create();
+
+    glm::mat4 proj = glm::perspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
+
+    clock->start();
+
+    bool running = true;
+    while (running) {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            if (ev.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        double elapsed = clock->getTime();
+        model->update(elapsed, camera->getView(), proj);
+
+        window->clear();
+        model->draw();
+        window->swap();
+    }
+
+    Model::Delete(model);
+    Window::Destroy(window.get());
+
+    SDL_Quit();
+    return 0;
+}
+
+std::shared_ptr<Model> Model::Create(void)
+{
+    std::shared_ptr<Model> model(new Model());
+    std::string modeldata = load_text_file("./cube.json");
+
+    model->m_mesh = TDRMesh::Load2(modeldata.c_str(), modeldata.length());
+    std::stringstream vsp;
+    vsp << "./shaders/" << model->m_mesh->getData(0,
+        TDRMesh::VertexShaderPath);
+    std::string vshader = load_text_file(vsp.str());
+
+    std::stringstream fsp;
+    fsp << "./shaders/" << model->m_mesh->getData(0,
+        TDRMesh::FragmentShaderPath);
+    std::string fshader = load_text_file(fsp.str());
+
+    std::stringstream texp;
+    texp << "./textures/" << model->m_mesh->getData(0,
+        TDRMesh::TexturePath);
+
+    std::shared_ptr<DirectDrawTexture> ddt(new DirectDrawTexture(texp.str()));
+    model->m_texture = std::shared_ptr<GLTexture>(GLTexture::Create(ddt));
+
+    model->m_program = ShaderProgram::Create();
+    model->m_program->attachShader(GL_VERTEX_SHADER, vshader);
+    model->m_program->attachShader(GL_FRAGMENT_SHADER, fshader);
+    if (!model->m_program->link()) {
+#ifdef TDR_DEBUG
+        std::cerr << "Failed to link shader code.  Aborting." << std::endl;
+#endif
+        return nullptr;
+    }
+
+    model->m_uv_attrib = model->m_program->getAttributeLocation("vertexUV");
+    model->m_vpos_attrib = model->m_program->getAttributeLocation(
+        "vertexPosition_modelspace");
+    model->m_mvp_uniform = model->m_program->getUniformLocation("MVP");
+
+    if (model->m_uv_attrib == -1 || model->m_vpos_attrib == -1 ||
+        model->m_mvp_uniform == -1) {
+#ifdef TDR_DEBUG
+        if (model->m_uv_attrib == -1) {
+            std::cerr << "Failed to find UV attribute." << std::endl;
+        }
+
+        if (model->m_vpos_attrib == -1) {
+            std::cerr << "Failed to find vertex position attribute.";
+            std::cerr << std::endl;
+        }
+
+        if (model->m_mvp_uniform == -1) {
+            std::cerr << "Failed to find MVP matrix uniform." << std::endl;
+        }
+#endif
+        return nullptr;
+    }
+
+    std::vector<float> verts = model->m_mesh->get(TDRMesh::Vertex);
+    std::vector<float> uvs = model->m_mesh->get(TDRMesh::UV);
+
+#ifdef TDR_DEBUG
+    model->dump_triplets(verts);
+    model->dump_doubles(uvs);
+#endif
+
+    glGenBuffers(1, &model->m_vbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, model->m_vbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * verts.size(), verts.data(),
+        GL_STATIC_DRAW);
+
+    glGenBuffers(1, &model->m_uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, model->m_uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * uvs.size(), uvs.data(),
+        GL_STATIC_DRAW);
+
+    model->m_pos = glm::vec3(0.0f, 0.0f, 0.0f);
+    model->m_model = glm::translate(glm::mat4(1.0f), model->m_pos);
+
+    model->direction = 'u';
+    model->last = 0.0;
+
+    return model;
+}
+
+void Model::Delete(std::shared_ptr<Model> model)
+{
+    glDeleteBuffers(1, &model->m_vbuffer);
+    glDeleteBuffers(1, &model->m_uvbuffer);
+    ShaderProgram::Destroy(model->m_program);
+}
+
+bool Model::addTexture(std::shared_ptr<DirectDrawTexture> ddt)
+{
+    m_texture = std::shared_ptr<GLTexture>(GLTexture::Create(ddt));
+    if (!m_texture->isValid()) {
+        return false;
+    }
+
+    return true;
+}
+
+void Model::draw(void)
+{
+    glUseProgram(m_program->getProgram());
+    glUniformMatrix4fv(m_mvp_uniform, 1, GL_FALSE, glm::value_ptr(m_mvp));
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_texture->getID());
+    glUniform1i(m_tex_uniform, 0);
+
+    glEnableVertexAttribArray(m_vpos_attrib);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbuffer);
+    glVertexAttribPointer(m_vpos_attrib, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(m_uv_attrib);
+    glBindBuffer(GL_ARRAY_BUFFER, m_uvbuffer);
+    glVertexAttribPointer(m_uv_attrib, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glDrawArrays(GL_TRIANGLES, 0, 12*3);
+    glDisableVertexAttribArray(m_vpos_attrib);
+    glDisableVertexAttribArray(m_uv_attrib);
+}
+
+glm::vec3 Model::getPosition(void)
+{
+    glm::vec4 col = glm::column(m_model, 3);
+    return glm::vec3(col.x, col.y, col.z);
+}
+
+bool Model::setPosition(glm::vec3 position)
+{
+    m_model = glm::translate(glm::mat4(1.0f), position);
+    return true;
+}
+
+bool Model::translate(glm::vec3 offset)
+{
+    m_model = glm::translate(m_model, offset);
+    return true;
+}
+
+void Model::update(double elapsed, glm::mat4 view, glm::mat4 proj)
+{
+    glm::vec3 position = getPosition();
+    double delta = elapsed - last;
+    float change = static_cast<float>(delta) * 2.0f;
+
+    /* outrageous logic to ratchet back and forth on the Y axis. */
+    if (direction == 'u') {
+        position.y += change;
+    } else if (direction == 'd') {
+        position.y -= change;
+    }
+
+    if (position.y > 1.0f) {
+        position.y = 1.0f;
+        direction = 'd';
+    } else if (position.y < -1.0f) {
+        position.y = -1.0f;
+        direction = 'u';
+    }
+
+    this->setPosition(position);
+    m_mvp = proj * view * m_model;
+    last = elapsed;
+}
+
+void Model::dump_triplets(std::vector<float> arr)
+{
+    for (size_t i = 0; i < arr.size(); i++) {
+        if ((i != 0) && (i % 3 == 0)) {
+            std::cout << std::endl;
+        }
+    
+        fprintf(stdout, "%.4f, ", arr[i]);
+    }
+    std::cout << std::endl;
+}
+
+void Model::dump_doubles(std::vector<float> arr)
+{
+    for (size_t i = 0; i < arr.size(); i++) {
+        if ((i != 0) && (i % 2 == 0)) {
+            std::cout << std::endl;
+        }
+
+        fprintf(stdout, "%.4f, ", arr[i]);
+    }
+    std::cout << std::endl;
+}
