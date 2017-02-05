e6118602748cd937345ecd286e719c6c64323cd0
Joe Brown <brownj3199@live.com>
2017-02-05 00:44:01 -0600

    tdrmeshtool work
    The MeshGenerator is now generating *something*, but nothing really
    useful.  For now, the constructor will generate some sort of default
    mesh that can be rendered in a test as a proof of concept, but will
    be expanded to do a variety of things.

--- tools/tdrmeshtool/include/meshgenerator.h
+++ tools/tdrmeshtool/include/meshgenerator.h
@@ -2,6 +2,8 @@
 #define TDRMESHTOOL_MESHGENERATOR_H
 
 #include <iostream>
+#include <memory>
+
 #include <tdrmain.h>
 
 class MeshGenerator {
@@ -13,9 +15,13 @@ public:
 
     MeshGenerator(void);
     virtual ~MeshGenerator(void);
+    /* TODO: Just as a proof of concept while I'm working this, I'll have the
+    * MeshGenerator simply generate my cube in the constructor.
     void generate(MeshGenerator::Simple object);
-    void interactive(void);
+    void interactive(void); */
+    std::string print(void);
 private:
+    std::unique_ptr<txml::XMLDocument> m_doc;
 };
 
 #endif
--- tools/tdrmeshtool/meshgenerator.cpp
+++ tools/tdrmeshtool/meshgenerator.cpp
@@ -1,1 +1,13 @@
 #include "meshgenerator.h"
+
+MeshGenerator::MeshGenerator(void)
+{
+    m_doc = std::unique_ptr<txml::XMLDocument>(new txml::XMLDocument());
+}
+
+MeshGenerator::~MeshGenerator(void) { }
+
+std::string MeshGenerator::print(void)
+{
+    return std::string("<scene>\n</scene>");
+}
